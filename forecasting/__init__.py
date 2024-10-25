04e35faf7bcecc7b9a040284d00bfd184f2cacc4
tuantong <76169085+tuantong@users.noreply.github.com>
2024-10-26 00:36:16 +0700

    Update __init__.py

--- forecasting/__init__.py
+++ forecasting/__init__.py
@@ -25,7 +25,7 @@ darts_flavor = LazyLoader(
 )
 
 # Define some variables to use accross the project
-PROJECT_ROOT_PATH = os.environ["LAMBDA_TASK_ROOT"] # custom-event
+PROJECT_ROOT_PATH = os.environ["HOME"] # custom-event
 # PROJECT_ROOT_PATH = "/Users/Apple/Desktop/CBI/custom-event/docker"
 PROJECT_SRC_PATH = os.path.join(PROJECT_ROOT_PATH, "log")
 PROJECT_DATA_PATH = os.path.join(PROJECT_ROOT_PATH, "data")
